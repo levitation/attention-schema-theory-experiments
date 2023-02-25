94c839e6d48039ae364dc74b12d227955cd93553
a.kochanke <a.kochanke@posteo.net>
2023-02-25 18:18:36 +0100

    rename path into path_from_root

--- aintelope/config/config_utils.py
+++ aintelope/config/config_utils.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from omegaconf import OmegaConf
 
 
-def get_project_path(path: str) -> Path:
+def get_project_path(path_from_root: str) -> Path:
     project_root = Path(__file__).parents[2]
-    return project_root / path
+    return project_root / path_from_root
 
 
 def register_resolvers() -> None:
